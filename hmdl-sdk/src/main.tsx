634b3b4ea32ec809095f373ea62fb75d69d84631
Riley-26 <rileyread1@gmail.com>
2025-09-18 01:00:43 +0100

    .

--- hmdl-sdk/src/main.tsx
+++ hmdl-sdk/src/main.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HmdlWidget } from '.';
+import { HmdlClient, HmdlWidget } from '.';
 
-const App: React.FC = () => (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <HmdlWidget 
-            apiKey={'1'}
-            checkedState={true}
-            theme={'dark'}
-        />
-    </div>
-);
+const App: React.FC = () => {
+    const hmdl = new HmdlClient({apiKey:"1", baseUrl:"yeah"})
+
+    hmdl.analyse("text")
+
+    return (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <HmdlWidget 
+                apiKey={'1'}
+                checkedState={true}
+                theme={'dark'}
+            />
+        </div>
+    )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
